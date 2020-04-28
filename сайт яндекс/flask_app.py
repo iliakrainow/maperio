e4d4464d6ae289155b75ca2c06b44555fee9b3e4
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2020-04-28 14:36:53 +0400

    Add files via upload

--- сайт яндекс/flask_app.py
+++ сайт яндекс/flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for
-from data import db_session, add_user_api, users
+from data import db_session, add_user_api, users, sessions
+import time
 import json
 import hashlib
 import requests
@@ -22,18 +23,32 @@ def index():
     elif request.method == 'POST':
         print(add_user_api.get_users(request.form["user"]))
         if add_user_api.get_users(request.form["user"]) == '{}':
+            nick = request.form["user"].lower()
             user = users.User()
-            user.name = request.form["user"].lower()
+            user.name = nick
             user.hashed_password = hashlib.md5(bytes(request.form["hashed_password"], 'utf-8')).hexdigest()
 
             session = db_session.create_session()
             session.add(user)
+            session.commit()
+
+            sess = sessions.Session()
+            sess.name = nick
+            sess.hashed = hashlib.md5(bytes(nick + str(time.time()), 'utf-8')).hexdigest()
+            session = db_session.create_session()
+            session.add(sess)
             session.commit()
             return render_template('index.html', style=url_for('static', filename='css/main.css'), text_in_login="OK")
         else:
             nick = request.form["user"]
             d = dict(json.loads(add_user_api.get_users(nick)))
             if hashlib.md5(bytes(request.form["hashed_password"], 'utf-8')).hexdigest() == str(d[str(nick)]):
+                sess = sessions.Session()
+                sess.name = nick
+                sess.hashed = hashlib.md5(bytes(nick + str(time.time()), 'utf-8')).hexdigest()
+                session = db_session.create_session()
+                session.add(sess)
+                session.commit()
                 return render_template('index.html', style=url_for('static', filename='css/main.css'), text_in_login="Успешно!")
             else:
                 return str(hash(request.form["hashed_password"])) + ' ' + str(d[str(nick)])
@@ -43,19 +58,36 @@ def index():
 def geo():
     return render_template('get_geo.html')
 
-@app.route('/score')
+@app.route('/score', methods=['GET', 'POST'])
 def score():
-    session = db_session.create_session()
-    sb = dict()
-    k = 0
-    for user in session.query(users.User).all():
-        k += 1
-        if user.score in sb:
-            sb[user.score] = sb[user.score] + ', ' + user.name
+    if request.method == 'GET':
+        session = db_session.create_session()
+        sb = dict()
+        k = 0
+        for user in session.query(users.User).all():
+            k += 1
+            if user.score in sb:
+                sb[user.score] = sb[user.score] + ', ' + user.name
+            else:
+                sb[user.score] = user.name
+        return render_template('score.html', style=url_for('static', filename='css/score.css'), sb=sb,
+                               sb2=sorted(sb, reverse=True)[:32])
+    elif request.method == 'POST':
+        session = db_session.create_session()
+        ok = 0
+        for s in session.query(sessions.Session).all():
+            print(s.hashed)
+            if s.hashed == request.form["session"] and s.name == request.form["user"].lower():
+                ok = 1
+                break
+
+        if ok:
+            session = db_session.create_session()
+            session.query(users.User).filter_by(name=request.form["user"].lower()).update({'score': int(request.form["score"])})
+            session.commit()
+            return 'ok'
         else:
-            sb[user.score] = user.name
-    return render_template('score.html', style=url_for('static', filename='css/score.css'), sb=sb,
-                           sb2=sorted(sb, reverse=True)[:32])
+            return 'False session id'
 
 
 @app.route('/store')
