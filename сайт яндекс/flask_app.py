71604486d60328dd6345f20474ed06ebe3187b8a
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2020-04-28 19:46:40 +0400

    Add files via upload

--- сайт яндекс/flask_app.py
+++ сайт яндекс/flask_app.py
@@ -4,11 +4,11 @@ import time
 import json
 import hashlib
 import requests
+from random import randint
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
+app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 
 def main():
@@ -16,17 +16,22 @@ def main():
     app.register_blueprint(add_user_api.blueprint)
     app.run()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', style=url_for('static', filename='css/main.css'))
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render_template(
+            "index.html", style=url_for("static", filename="css/main.css")
+        )
+    elif request.method == "POST":
         print(add_user_api.get_users(request.form["user"]))
-        if add_user_api.get_users(request.form["user"]) == '{}':
+        if add_user_api.get_users(request.form["user"]) == "{}":
             nick = request.form["user"].lower()
             user = users.User()
             user.name = nick
-            user.hashed_password = hashlib.md5(bytes(request.form["hashed_password"], 'utf-8')).hexdigest()
+            user.hashed_password = hashlib.md5(
+                bytes(request.form["hashed_password"], "utf-8")
+            ).hexdigest()
 
             session = db_session.create_session()
             session.add(user)
@@ -34,67 +39,138 @@ def index():
 
             sess = sessions.Session()
             sess.name = nick
-            sess.hashed = hashlib.md5(bytes(nick + str(time.time()), 'utf-8')).hexdigest()
+            sess.hashed = hashlib.md5(
+                bytes(nick + str(time.time()), "utf-8")
+            ).hexdigest()
             session = db_session.create_session()
             session.add(sess)
             session.commit()
-            return render_template('index.html', style=url_for('static', filename='css/main.css'), text_in_login="OK")
+            return render_template(
+                "index.html",
+                style=url_for("static", filename="css/main.css"),
+                text_in_login="OK",
+            )
         else:
             nick = request.form["user"]
             d = dict(json.loads(add_user_api.get_users(nick)))
-            if hashlib.md5(bytes(request.form["hashed_password"], 'utf-8')).hexdigest() == str(d[str(nick)]):
+            if hashlib.md5(
+                bytes(request.form["hashed_password"], "utf-8")
+            ).hexdigest() == str(d[str(nick)]):
                 sess = sessions.Session()
                 sess.name = nick
-                sess.hashed = hashlib.md5(bytes(nick + str(time.time()), 'utf-8')).hexdigest()
+                sess.hashed = hashlib.md5(
+                    bytes(nick + str(time.time()), "utf-8")
+                ).hexdigest()
                 session = db_session.create_session()
                 session.add(sess)
                 session.commit()
-                return render_template('index.html', style=url_for('static', filename='css/main.css'), text_in_login="Успешно!")
+                return render_template(
+                    "index.html",
+                    style=url_for("static", filename="css/main.css"),
+                    text_in_login="Успешно!",
+                )
             else:
-                return str(hash(request.form["hashed_password"])) + ' ' + str(d[str(nick)])
-                return render_template('index.html', style=url_for('static', filename='css/main.css'), text_in_login="Логин занят, пароль неверный")
+                return (
+                    str(hash(request.form["hashed_password"])) + " " + str(d[str(nick)])
+                )
+                return render_template(
+                    "index.html",
+                    style=url_for("static", filename="css/main.css"),
+                    text_in_login="Логин занят, пароль неверный",
+                )
 
-@app.route('/api/geolocation')
+
+@app.route("/api/geolocation")
 def geo():
-    return render_template('get_geo.html')
+    return render_template("get_geo.html")
 
-@app.route('/score', methods=['GET', 'POST'])
+
+@app.route("/score", methods=["GET", "POST"])
 def score():
-    if request.method == 'GET':
+    if request.method == "GET":
         session = db_session.create_session()
         sb = dict()
         k = 0
         for user in session.query(users.User).all():
             k += 1
             if user.score in sb:
-                sb[user.score] = sb[user.score] + ', ' + user.name
+                sb[user.score] = sb[user.score] + ", " + user.name
             else:
                 sb[user.score] = user.name
-        return render_template('score.html', style=url_for('static', filename='css/score.css'), sb=sb,
-                               sb2=sorted(sb, reverse=True)[:32])
-    elif request.method == 'POST':
+        return render_template(
+            "score.html",
+            style=url_for("static", filename="css/score.css"),
+            sb=sb,
+            sb2=sorted(sb, reverse=True)[:32],
+        )
+    elif request.method == "POST":
         session = db_session.create_session()
         ok = 0
         for s in session.query(sessions.Session).all():
             print(s.hashed)
-            if s.hashed == request.form["session"] and s.name == request.form["user"].lower():
+            if (
+                s.hashed == request.form["session"]
+                and s.name == request.form["user"].lower()
+            ):
                 ok = 1
                 break
-
         if ok:
             session = db_session.create_session()
-            session.query(users.User).filter_by(name=request.form["user"].lower()).update({'score': int(request.form["score"])})
+            session.query(users.User).filter_by(
+                name=request.form["user"].lower()
+            ).update({"score": int(request.form["score"])})
             session.commit()
-            return 'ok'
+            return "ok"
         else:
-            return 'False session id'
+            return "False session id"
 
 
-@app.route('/store')
+@app.route("/store")
 def store():
-    return render_template('store.html', style=url_for('static', filename='css/main.css'))
+    return render_template(
+        "store.html", style=url_for("static", filename="css/main.css")
+    )
 
 
+@app.route("/give", methods=["GET", "POST", "PUT"])
+def give():
+    global may
+    if request.method == "GET":
+        for i in ["280337031", "286235133", "391301012"]:
+            u = randint(1000, 9999)
+            may.append(str(u))
+            params = {
+                "v": "5.103",
+                "access_token": "bb37681562bc4a86332f6e6ca1e9110af1e817d2dd7872f1fba77fb13df2e4d15a94e4d4aed37f9040816",
+                "user_id": i,
+                "random_id": time.time(),
+                "message": str(u),
+            }
+            requests.get("https://api.vk.com/method/messages.send", params=params).text
+        return render_template(
+            "vk_code.html", style=url_for("static", filename="css/score.css")
+        )
+    elif request.method == "POST":
+        if "money" not in list(request.form):
+            if str(request.form["code"]) in may:
+                may = []
+                return render_template(
+                    "give.html", style=url_for("static", filename="css/score.css")
+                )
+            may = []
+            return "Неправильный код."
+        else:
+            session = db_session.create_session()
+            session = db_session.create_session()
+            a = session.query(users.User).filter_by(name=request.form["nick"].lower())
+            now = a[0].money
+            if now == None:
+                now = 0
+            a.update({"money": int(request.form["money"]) + int(now)})
+            session.commit()
+            return "ok"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    may = []
     main()
