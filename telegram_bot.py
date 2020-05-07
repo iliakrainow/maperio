3657a10f01c38d04417767ad4d2ce3e76fcf7fc2
LokirTreg <57491076+LokirTreg@users.noreply.github.com>
2020-05-07 16:02:29 +0400

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,8 +2,18 @@ import telebot
 
 bot = telebot.TeleBot('1242649621:AAH0aGPCowr8TVQ2F-zizFJn3JZK0qLkd5Y')
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
 
 bot.polling(none_stop=True, timeout=123)
