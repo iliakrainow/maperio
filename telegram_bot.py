e498b40ec0c3d1f9d1dd6115d4c7a772383c8f0d
LokirTreg <57491076+LokirTreg@users.noreply.github.com>
2020-05-08 19:30:18 +0400

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,44 +1,74 @@
 # Импортируем необходимые классы.
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+from telegram import ReplyKeyboardMarkup
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, сам бот и класс updater, принявший сообщение.
 def echo(update, context):
-    # У объекта класса Updater есть поле message, 
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str), 
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    update.message.reply_text(update.message.text)
+    update.message.reply_text('''Error 00000000000000000000000000000
+    A problem has been detected and Android has been shut down to prevent damage
+to your computer.
+
+The problem seems to be caused by the following file: xNtKrnl.exe
+
+SYSTEM_THREAD_EXCEPTION_NOT_HANDLED
+
+If this is the first time you've seen this stop error screen,
+restart your computer. If this screen appears again, follow
+these steps:
+
+Check to make sure any new hardware or software is properly installed.
+If this is a new installation, ask your hardware or software manufacturer
+for any Windows updates you might need.
+
+If problems continue, disable or remove any newly installed hardware
+or software. Disable BIOS memory options such as caching or shadowing.
+If you need to use safe mode to remove or disable components, restart
+your computer, press F8 to select Advanced Startup Options, and then
+select Safe Mode.
+
+Technical Information:
+
+*** STOP: 0x1000007e (0xffffffffc0000005, 0xfffff80002e55151, 0xfffff880009a99d8,
+0xfffff880009a9230)
+
+*** xNtKrnl.exe - Address 0xfffff80002e55151 base at 0xfffff80002e0d000 DateStamp
+0x4ce7951a
+''')
 
 
 def main():
-    # Создаём объект updater. 
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    updater = Updater('1242649621:AAH0aGPCowr8TVQ2F-zizFJn3JZK0qLkd5Y', use_context=True)
+    REQUEST_KWARGS = {
+        'proxy_url': 'socks5://148.251.234.93:1080'}
 
-    # Получаем из него диспетчер сообщений.
+    updater = Updater('1242649621:AAH0aGPCowr8TVQ2F-zizFJn3JZK0qLkd5Y', use_context=True,
+                      request_kwargs=REQUEST_KWARGS)
+
     dp = updater.dispatcher
 
-    # Создаём обработчик сообщений типа Filters.text 
-    # из описанной выше функции echo()
-    # После регистрации обработчика в диспетчере
-    # эта функция будет вызываться при получении сообщения 
-    # с типом "текст", т. е. текстовых сообщений.
+    def start(update, context):
+        update.message.reply_text(
+            "Привет! Че надо?")
+
+    def help(update, context):
+        update.message.reply_text(
+            "Комманды: /help, /phone, /site")
+
+    def phone(update, context):
+        update.message.reply_text("Телефон: Не дам")
+
+    def site(update, context):
+        update.message.reply_text(
+            "Сайт: Нема")
+    dp.add_handler(CommandHandler("phone", phone))
+    dp.add_handler(CommandHandler("site", site))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
     text_handler = MessageHandler(Filters.text, echo)
-
-    # Регистрируем обработчик в диспетчере.
     dp.add_handler(text_handler)
-    # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
-
-    # Ждём завершения приложения. 
-    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
